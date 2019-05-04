66bfadcfeaff9d9d59070d6b97b9ff631b3ecd02
marlon marin <marlon.marin@uao.edu.co>
2019-05-04 18:03:51 -0500

    Updates

---
+++ precache-manifest.0c0450403a5a53f1fc50ee2737e2a997.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9bf413d4fce9fe7166ce697ddaa4d7a",
+    "url": "/PokeExport/index.html"
+  },
+  {
+    "revision": "cd59b61a8aec19250ec7",
+    "url": "/PokeExport/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "424260c91c82d9eb7bb5",
+    "url": "/PokeExport/static/css/3.602e8328.chunk.css"
+  },
+  {
+    "revision": "326aa2ae3c9713ae06bc",
+    "url": "/PokeExport/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "cd59b61a8aec19250ec7",
+    "url": "/PokeExport/static/js/2.e01633bc.chunk.js"
+  },
+  {
+    "revision": "424260c91c82d9eb7bb5",
+    "url": "/PokeExport/static/js/3.badbfd45.chunk.js"
+  },
+  {
+    "revision": "326aa2ae3c9713ae06bc",
+    "url": "/PokeExport/static/js/main.2eb2d656.chunk.js"
+  },
+  {
+    "revision": "15bb26f08b20cea0772e",
+    "url": "/PokeExport/static/js/runtime~main.6cd557db.js"
+  },
+  {
+    "revision": "a400a87be6bb065ec79c7b4666e26a0c",
+    "url": "/PokeExport/static/media/FondoSonido.a400a87b.mp3"
+  },
+  {
+    "revision": "65d5511af857787003d5201a4af85b87",
+    "url": "/PokeExport/static/media/bg.65d5511a.png"
+  }
+]);
